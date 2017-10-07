a42d5a235db5f1b47ea117dd98588dbe9a84cc6c
Ken Sin <kensin1129@gmail.com>
2017-10-07 00:38:35 -0400

    Update contract service with ember concurrency task

--- blueprints/import-contracts/files/app/services/__contractServiceToken__.js
+++ blueprints/import-contracts/files/app/services/__contractServiceToken__.js
@@ -4,11 +4,12 @@ import <%= entityName %>Artifacts from '<%= dasherizedPackageName %>/contracts/<
 import { default as contract } from 'npm:truffle-contract';
 import computed from 'ember-computed';
 import get from 'ember-metal/get';
+import { task } from 'ember-concurrency';
 
 export default Service.extend({
   web3: inject(),
 
-  instance: computed({
+  instance: computed('web3.instance', {
     get() {
       const currentProvider = get(this, 'web3.instance').currentProvider;
       const contractInstance = contract(<%= entityName %>Artifacts);
@@ -16,5 +17,9 @@ export default Service.extend({
       contractInstance.setProvider(currentProvider);
       return contractInstance;
     }
+  }),
+
+  deployed: task(function * () {
+    return yield get(this, 'instance').deployed();
   })
 });
